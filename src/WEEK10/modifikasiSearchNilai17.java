90520ebb62e7ed502e9077a6b74d07ce091e2fc9
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2024-10-31 15:43:02 +0700

    31/10/2024

---
+++ src/WEEK10/modifikasiSearchNilai17.java
@@ -0,0 +1,38 @@
+package WEEK10;
+import java.util.Scanner;
+public class modifikasiSearchNilai17 {
+        public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int key=0 , hasil = 0 ,bnykInput;
+
+        System.out.print("Masukkan banyak nilai yang akan diinput: ");
+        bnykInput = sc.nextInt();
+        int[] arrayNilai = new int[bnykInput];
+
+        
+        for (int i = 0; i < arrayNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            arrayNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = sc.nextInt();
+        
+        for (int i = 0; i < arrayNilai.length; i++) {
+            if (key == arrayNilai[i]) {
+                hasil = i+1;
+                break;
+            }
+        }
+        if (hasil!=0) {
+            System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+hasil);
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
+        
+        System.out.println();
+        sc.close();
+    }
+}
+
+
---
+++ src/WEEK10/searchNilai17.java
@@ -0,0 +1,22 @@
+package WEEK10;
+
+import java.util.Scanner;
+
+public class searchNilai17 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int arrayNilai[] = {80 , 85 , 78 , 96, 90,  82 , 86};
+        int key = 90, hasil = 0 ;
+
+        for (int i = 0; i < arrayNilai.length; i++) {
+            if (key == arrayNilai[i]) {
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Nilai "+key+" ketemu di index ke-"+hasil);
+        System.out.println();
+        sc.close();
+    }
+}
