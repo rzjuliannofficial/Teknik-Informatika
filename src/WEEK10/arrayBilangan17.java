0c3ad8f597206ac3694ff3adad994fceba346d7b
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2024-10-31 12:37:25 +0700

    31/10/2024

--- src/WEEK10/arrayBilangan17.java
+++ src/WEEK10/arrayBilangan17.java
@@ -2,17 +2,27 @@ package WEEK10;
 
 public class arrayBilangan17 {
     public static void main(String[] args) {
-        
-        int bil[]= new int[4];
-        bil[0]=5;
-        bil[1]=13;
-        bil[2]=-7;
-        bil[3]=17;
+    
+        //v1 
+        // int bil[]= new int[4];
+        // bil[0]=5.0;
+        // bil[1]=13;
+        // bil[2]=-7;
+        // bil[3]=17;
+
+        //inisialisasi elemen array
+        int bil[]={5, 2 , 3, 13};
     
         System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
         System.out.println(bil[3]);
+        System.err.println();
+
+        // ubah output menggunakan for
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
 
