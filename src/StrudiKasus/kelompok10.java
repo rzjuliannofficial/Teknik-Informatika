dc36492fa408a6b8b230b1b8e09d7ca263b67ca2
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2024-12-08 12:38:56 +0700

    update

--- src/StrudiKasus/kelompok10.java
+++ src/StrudiKasus/kelompok10.java
@@ -8,10 +8,10 @@ public class kelompok10 {
   static String[] nim = new String[100]; // membatasi 100 nim
   static String[][] namaMk = new String[100][100]; // membatasi 100
   static String[][] kodeMk = new String[100][100]; // membatasi 26 kodematkul
-  static byte [][] sks = new byte[100][100]; 
-  static int i = 0 ;
+  static byte[][] sks = new byte[100][100]; 
   static int[] totalSks = new int[100];
   static int jumlahMahasiswa;
+  static int i = 0 ;
 
   public static void main(String[] args) {
     Menu();
@@ -60,6 +60,7 @@ public class kelompok10 {
     namaMahasiswa[i] = input10.nextLine();
     System.out.print("NIM: ");
     nim[i] = input10.nextLine();
+    
     int j =0;
     String jawaban;
 
@@ -72,12 +73,18 @@ public class kelompok10 {
       System.out.print("Jumlah SKS (1-3): ");
       sks[i][j] = input10.nextByte();
       input10.nextLine();
+      
         if (totalSks[i] <= 24) {
-          if (sks[i][j] > 3 || sks[i][j] < 1) { 
+          if (sks[i][j] > 3 || sks[i][j] < 1 ) { 
               System.out.println("Mohon isi yang benar");
               continue; // Kembali ke awal loop
           } else {
               totalSks[i] += sks[i][j];
+              if (totalSks[i]>24) {
+                System.out.println("SKS telah melebihi 24");
+                totalSks[i] -= sks[i][j];
+                break;
+              }
               System.out.println("Data Mata Kuliah Berhasil Ditambahkan");
               System.out.print("Tambah mata kuliah lagi? (y/n): ");
               jawaban = input10.nextLine();
@@ -86,13 +93,8 @@ public class kelompok10 {
                   break; // Keluar dari loop
               }
               j++; 
-          }
-        } else {
-          j--; //untuk mengurangi j yang telah ditambah
-          System.out.println("SKS telah lebih dari 24");
-          totalSks[i] -= sks[i][j]; //untuk mengurangi sks yang telah ditambah
-          break; // Keluar dari loop
-      } 
+            }
+        }
     }
     System.out.println("Total SKS: "+totalSks[i]);
     if (totalSks[i]<20) {
@@ -107,27 +109,29 @@ public class kelompok10 {
     System.out.print("Masukkan Nim mahasiswa: ");
     String NIM = input10.nextLine();
     System.out.println("Daftar KRS");
+    boolean dataAda=false;
     for (int i = 0; i < sks.length; i++) {
       if (NIM.equals(nim[i])) {
-        System.out.println("Nim ||\t\t|| Nama ||\t|| Kode MK ||\t|| Nama Mata Kuliah ||\t|| SKS");
+        dataAda = true;
+        System.out.println("\nNim \t\t Nama \t Kode MK \t Nama Mata Kuliah SKS");
         for (int j = 0; j < nim.length; j++) {
           if (kodeMk[i][j] == null) {
             break;
           }
-          System.out.print("\n"+nim[i]+"||\t||"+namaMahasiswa[i]+"||\t||"+kodeMk[i][j]+"||\t\t||"+namaMk[i][j]+"||\t||"+sks[i][j]);
+          System.out.print(nim[i]+"\t"+namaMahasiswa[i]+"\t"+kodeMk[i][j]+"\t\t"+namaMk[i][j]+"\t\t"+sks[i][j]);
         }
         System.out.println();   
         System.out.println("Total SKS: "+totalSks[i]);
         System.out.println();
-      }
+        break;
+        }
+    }
+      if (!dataAda) { //jadi jika data tidak ada, kondisi dalam if ini akan berjalan
+        System.out.println("Data dengan nim "+NIM+" tidak ditemukan");
     }
   }
-      
-    
   
-
   public static void menu3() {
   System.out.println("Jumlah mahasiswa yang mengambil SKS kurang dari 20: "+jumlahMahasiswa);
   }
-  
 }
