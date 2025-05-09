37c6c5dcc7dcb19892dfb216bd69ff97e3c2ee4e
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-09 19:36:10 +0700

    add fungsi main and add switch case in the main

--- src/Semester2/WEEK11/Praktikum1/QueueMain17.java
+++ src/Semester2/WEEK11/Praktikum1/QueueMain17.java
@@ -1,5 +1,8 @@
 package WEEK11.Praktikum1;
 
+import java.util.Queue;
+import java.util.Scanner;
+
 public class QueueMain17 {
 
     public static void menu(){
@@ -10,5 +13,43 @@ public class QueueMain17 {
         System.out.println("4. Peek");
         System.out.println("5. Clear");
         System.out.println("-----------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan kapasitas queue:");
+        int n = sc.nextInt();
+        Queue<E> Q = new QueueMain17(n);
+        int pilih = 0;
+
+        do {
+            menu();
+            pilih = sc.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan data baru: ");
+                    int dataMasuk = sc.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.Dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data yang dikeluarkan: " + dataKeluar);
+                    }
+                    break;
+                case 3:
+                    Q.print();
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+                default:
+                    break;
+            }
+        } while (pilih > 0 && pilih <= 5);
     }
 }
