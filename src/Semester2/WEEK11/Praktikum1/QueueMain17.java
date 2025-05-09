6dc6b3740deddbff1a1e89bee2cea90631f4d36a
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-09 19:18:22 +0700

    add method Menu in MainClass

--- src/Semester2/WEEK11/Praktikum1/QueueMain17.java
+++ src/Semester2/WEEK11/Praktikum1/QueueMain17.java
@@ -1,7 +1,14 @@
 package WEEK11.Praktikum1;
 
 public class QueueMain17 {
-    public static void main(String[] args) {
-        
+
+    public static void menu(){
+        System.out.println("Masukkan operasi yang diiinginkan:");
+        System.out.println("1. Enqueue");
+        System.out.println("2. Dequeuq");
+        System.out.println("3. Print");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("-----------------");
     }
 }
