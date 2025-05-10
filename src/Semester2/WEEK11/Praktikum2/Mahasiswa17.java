e03865f326020ad1dd8088217642d79a74d54aa6
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-10 11:55:08 +0700

    add konstraktor and function tampilkanData in Class Mahasiswa17

--- src/Semester2/WEEK11/Praktikum2/Mahasiswa17.java
+++ src/Semester2/WEEK11/Praktikum2/Mahasiswa17.java
@@ -1,5 +1,19 @@
 package WEEK11.Praktikum2;
 
 public class Mahasiswa17 {
-    
+    String nama;
+    String nim;
+    String prodi;
+    String kelas;
+
+    public Mahasiswa17(String nama, String nim, String prodi, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    public void tampilkanData(){
+        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas);
+    }
 }
