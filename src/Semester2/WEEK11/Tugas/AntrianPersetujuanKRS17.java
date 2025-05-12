1051afce35dc4564feceadca566d36300803042e
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-12 13:04:33 +0700

    add construktor in AntrianPersetujuanKRS17

--- src/Semester2/WEEK11/Tugas/AntrianPersetujuanKRS17.java
+++ src/Semester2/WEEK11/Tugas/AntrianPersetujuanKRS17.java
@@ -7,5 +7,11 @@ public class AntrianPersetujuanKRS17 {
     int size;
     int max;
 
-    
+    public AntrianPersetujuanKRS17(int max){
+        this.max = max ;
+        this.data = new Mahasiswa17[max]; //inisialisasi panjang array data
+        this.size = 0;
+        this.front = 0; //index depan
+        this.rear = -1; //index belakang
+    }
 }
