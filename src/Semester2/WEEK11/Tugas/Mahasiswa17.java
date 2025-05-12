a48e0da25d27820c6b6703580a08c00861f53b54
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-12 12:56:20 +0700

    Add construktor for Class Mahasiswa17

--- src/Semester2/WEEK11/Tugas/Mahasiswa17.java
+++ src/Semester2/WEEK11/Tugas/Mahasiswa17.java
@@ -5,4 +5,11 @@ public class Mahasiswa17 {
     String nama;
     String prodi;
     String kelas;
+
+    public Mahasiswa17(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
 }
