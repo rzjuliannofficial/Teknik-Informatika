e6e12f1dafb6d536a3980851f8570d61e8e85399
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-12 12:57:04 +0700

    add fungsi tampilkanData

--- src/Semester2/WEEK11/Tugas/Mahasiswa17.java
+++ src/Semester2/WEEK11/Tugas/Mahasiswa17.java
@@ -6,10 +6,15 @@ public class Mahasiswa17 {
     String prodi;
     String kelas;
 
+
     public Mahasiswa17(String nim, String nama, String prodi, String kelas) {
         this.nim = nim;
         this.nama = nama;
         this.prodi = prodi;
         this.kelas = kelas;
+    }
+
+    public void tampilkanData(){
+        System.out.printf("%-15s|%-15s|%-15s|%-15s|%n", nim, nama, prodi, kelas);
     }
 }
