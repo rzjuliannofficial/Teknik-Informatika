fca267374a5c5398ecea9b2ad052190a88f966bd
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-29 14:50:20 +0700

    create and add function tampilkan informasi in Class Mahasiswa17

---
+++ src/Semester2/WEEK12/Mahasiswa17.java
@@ -0,0 +1,19 @@
+package WEEK12;
+
+public class Mahasiswa17 {
+    String nim;
+    String nama;
+    String kelas;
+    String ipk;
+
+    public Mahasiswa17(String nim, String nama, String kelas, String ipk) {
+     this.nim = nim;
+     this.nama = nama;
+     this.kelas = kelas;
+     this.ipk = ipk;
+    }
+
+    public void tampilkanInformasi(){
+        System.out.printf("%-15s %-15s %-15s %-15s %n", nama, nim, kelas, ipk);
+    }
+}
