c5eacbdd34e1b6f8c917ed0db5100179740c292d
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-29 16:55:54 +0700

    switch folder

---
+++ src/Semester2/WEEK12/Percobaan1/Node17.java
@@ -0,0 +1,11 @@
+package WEEK12.Percobaan1;
+
+public class Node17 {
+    Mahasiswa17 data;
+    Node17 next;
+
+    public Node17(Mahasiswa17 data, Node17 next){
+        this.data = data;
+        this.next = next;
+    }
+}
