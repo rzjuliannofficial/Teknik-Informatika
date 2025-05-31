f7e29950046aef2223d83af1ec844b8d0f77d642
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-31 09:28:48 +0700

    add class Node for linkedList

---
+++ src/Semester2/WEEK12/Tugas_AntrianQUEUE_LinkedList/Node17.java
@@ -0,0 +1,11 @@
+package WEEK12.Tugas_AntrianQUEUE_LinkedList;
+
+public class Node17 {
+    Mahasiswa17 data;
+    Node17 next;
+
+    public Node17(Mahasiswa17 data, Node17 next) {
+        this.data = data;
+        this.next = next;
+    }
+}
