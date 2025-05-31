16de01147657aaea66407da036ba029e35c35f31
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-31 09:34:58 +0700

    31/05/25 testing

--- src/Semester2/WEEK12/Tugas_AntrianQUEUE_LinkedList/SingleLinkedList_QUEUE.java
+++ src/Semester2/WEEK12/Tugas_AntrianQUEUE_LinkedList/SingleLinkedList_QUEUE.java
@@ -56,12 +56,6 @@ public class SingleLinkedList_QUEUE {
             return ;
         }
 
-        if (head == tail) {
-            head = null;
-            tail = null;
-        } else {
-            head = head.next;
-        }
         System.out.println("Mahasiswa terdepan telah dilayani dan dihapus dari antrian.");
         System.out.println("===================================================================+");
         System.out.printf("|%-15s|%-15s|%-15s|%-15s|%n", "nim", "nama", "prodi", "kelas");
@@ -69,6 +63,13 @@ public class SingleLinkedList_QUEUE {
         head.data.tampilkanData();
         System.out.println("===================================================================+");
         size--;
+        
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+        }
     }
 
     public void tampilkanAntrian() {
