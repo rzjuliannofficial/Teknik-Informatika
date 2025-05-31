8bb001e11591f2fe3421df400dbe06239327dd0a
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-31 09:32:48 +0700

    31/05/25

--- src/Semester2/WEEK12/Tugas_AntrianQUEUE_LinkedList/AntrianLinkedList17.java
+++ src/Semester2/WEEK12/Tugas_AntrianQUEUE_LinkedList/AntrianLinkedList17.java
@@ -30,9 +30,9 @@ public class AntrianLinkedList17 {
             switch (pilihan) {
                 case 1:
                     System.out.println("=== Tambah Antrian ===");
-                    System.out.print("Masukkan NIM:");
+                    System.out.print("Masukkan NIM: ");
                     String nim = sc.nextLine();
-                    System.out.print("Masukkan Nama:");
+                    System.out.print("Masukkan Nama: ");
                     String nama = sc.nextLine();
                     System.out.print("Masukkan Prodi: ");
                     String prodi = sc.nextLine();
