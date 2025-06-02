2bb4937a1ba637c259546c1bef95ff92ede20549
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-06-02 14:26:34 +0700

    add Node17

---
+++ src/Semester2/WEEK13/Node17.java
@@ -0,0 +1,13 @@
+package WEEK13;
+
+public class Node17 {
+    Mahasiswa17 data;
+    Node17 next;
+    Node17 prev;
+    
+    public Node17(Mahasiswa17 data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
