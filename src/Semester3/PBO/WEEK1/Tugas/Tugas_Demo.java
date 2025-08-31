956923862aa7863d35ee0fc106857f95c0cdf79a
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-08-31 12:42:37 +0700

    rename

---
+++ src/Semester3/PBO/WEEK1/Tugas/Tugas_Demo.java
@@ -0,0 +1,31 @@
+public class Tugas_Demo {
+    public static void main(String[] args) {
+        alasKaki alasKaki1 = new alasKaki();
+        alasKaki alasKaki2 = new alasKaki();
+        sepatuLari alasKaki3 = new sepatuLari();
+
+        alasKaki1.setTipe("sandal");
+        alasKaki1.setWarna("hitam");
+        alasKaki1.SNI(true);
+        alasKaki1.madeIn("China");
+        alasKaki1.gantiWarna("-");
+        alasKaki1.cetak();
+        System.out.println();
+
+        alasKaki2.setTipe("sepatu");
+        alasKaki2.setWarna("hitam-putih");
+        alasKaki2.SNI(true);
+        alasKaki2.madeIn("Indonesia");
+        alasKaki2.gantiWarna("-");
+        alasKaki2.cetak();
+        System.out.println();
+
+        alasKaki3.setTipe("sepatu");
+        alasKaki3.setWarna("putih");
+        alasKaki3.SNI(true);
+        alasKaki3.madeIn("US");
+        alasKaki3.gantiWarna("-");
+        alasKaki3.tipeSol("Carbon");
+        alasKaki3.cetak();
+    }
+}
