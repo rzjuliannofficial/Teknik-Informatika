5b9f81629ec68cbc2d6ac9c15cebdc78b653f904
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 08:32:33 +0700

    add method tampilBarang

--- src/Semester3/PBO/WEEK2/Percobaan3/Barang19.java
+++ src/Semester3/PBO/WEEK2/Percobaan3/Barang19.java
@@ -4,4 +4,10 @@ public class Barang19 {
     public String namaBrg;
     public String jenisBrg;
     public int stok;
+    
+    public void tampilBarang () {
+        System.out.println ("Nama Barang : "+namaBrg);
+        System.out.println ("Jenis Barang : "+jenisBrg);
+        System.out.println ("Stok : "+stok);
+    }
 }
