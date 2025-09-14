2631e87b1daad71bd5c6487326d1353e214ac965
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 10:24:13 +0700

    deklarasi atribut barang

---
+++ src/Semester3/PBO/WEEK2/Tugas/Barang/Barang19.java
@@ -0,0 +1,9 @@
+package Semester3.PBO.WEEK2.Tugas.Barang;
+
+public class Barang19 {
+    public String kode;
+    public String namaBarang;
+    public double hargaDasar;
+    public float diskon; // Diskon dalam persen
+
+}
