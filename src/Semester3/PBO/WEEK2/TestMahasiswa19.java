620ddebb0f56079cc95bfe74e716d256c860158a
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 08:25:35 +0700

    add 2 object more

--- src/Semester3/PBO/WEEK2/TestMahasiswa19.java
+++ src/Semester3/PBO/WEEK2/TestMahasiswa19.java
@@ -8,5 +8,21 @@ public class TestMahasiswa19 {
         mhs1.alamat="Jl. Vinolia No 1A";
         mhs1.kelas="1A";
         mhs1.tampilBiodata();
+        
+        Mahasiswa19 mhs2 = new Mahasiswa19();
+        mhs2.nim = 201;
+        mhs2.nama="Jokowi";
+        mhs2.alamat="Jl. Vinolia No 2A";
+        mhs2.kelas="2A";
+        mhs2.tampilBiodata();
+        
+        Mahasiswa19 mhs3 = new Mahasiswa19();
+        mhs3.nim = 301;
+        mhs3.nama="Praroro";
+        mhs3.alamat="Jl. Vinolia No 3A";
+        mhs3.kelas="3A";
+        mhs3.tampilBiodata();
+
+
     }    
 }
