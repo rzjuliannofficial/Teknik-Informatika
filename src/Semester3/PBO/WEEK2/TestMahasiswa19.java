e803119fd7cc51baa620e6568bc5d6d2824ff8e2
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 08:04:24 +0700

    inisialisasi object didalam main

---
+++ src/Semester3/PBO/WEEK2/TestMahasiswa19.java
@@ -0,0 +1,12 @@
+package Semester3.PBO.WEEK2;
+
+public class TestMahasiswa19 {
+    public static void main(String[] args) {
+        Mahasiswa19 mhs1 = new Mahasiswa19();
+        mhs1.nim = 101;
+        mhs1.nama="Lestari";
+        mhs1.alamat="Jl. Vinolia No 1A";
+        mhs1.kelas="1A";
+        mhs1.tampilBiodata();
+    }    
+}
