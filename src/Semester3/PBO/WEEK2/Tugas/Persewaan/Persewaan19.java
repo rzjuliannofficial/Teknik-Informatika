cc14b81477c99282b7c84b98ab2522dd2ed50fa0
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 10:07:14 +0700

    add method tampilData

--- src/Semester3/PBO/WEEK2/Tugas/Persewaan/Persewaan19.java
+++ src/Semester3/PBO/WEEK2/Tugas/Persewaan/Persewaan19.java
@@ -12,4 +12,14 @@ public class Persewaan19 {
         totalHarga = harga * lamaSewa;
         return totalHarga;
     }
+    
+    public void tampilData(){
+        System.out.println("ID Sewa: " + idSewa);
+        System.out.println("Nama Member: " + namaMember);
+        System.out.println("Nama Game: " + namaGame);
+        System.out.println("Harga Sewa: Rp " + harga);
+        System.out.println("Lama Sewa: " + lamaSewa + " hari");
+        System.out.println("---------------------------------");
+        System.out.println("Total Harga: Rp " + totalHarga);
+    }
 }
