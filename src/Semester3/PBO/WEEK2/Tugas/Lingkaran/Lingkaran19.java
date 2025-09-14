35febef87c96077db393a5758235343fa3aaf0bc
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 10:15:10 +0700

    add lingkaran19

---
+++ src/Semester3/PBO/WEEK2/Tugas/Lingkaran/Lingkaran19.java
@@ -0,0 +1,14 @@
+package Semester3.PBO.WEEK2.Tugas.Lingkaran;
+
+public class Lingkaran19 {
+    public double phi = 3.14;
+    public double r;
+
+    public double hitungLuas() {
+        return phi * r * r;
+    }
+
+    public double hitungKeliling() {
+        return 2 * phi * r;
+    }
+}
