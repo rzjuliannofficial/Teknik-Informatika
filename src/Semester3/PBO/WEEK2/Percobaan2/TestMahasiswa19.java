b5f06dcd62707ce9a654076644816d0d2414b124
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 08:28:14 +0700

    move to folder percobaan1

---
+++ src/Semester3/PBO/WEEK2/Percobaan2/TestMahasiswa19.java
@@ -0,0 +1,28 @@
+package Semester3.PBO.WEEK2.Percobaan2;
+
+public class TestMahasiswa19 {
+    public static void main(String[] args) {
+        Mahasiswa19 mhs1 = new Mahasiswa19();
+        mhs1.nim = 101;
+        mhs1.nama="Lestari";
+        mhs1.alamat="Jl. Vinolia No 1A";
+        mhs1.kelas="1A";
+        mhs1.tampilBiodata();
+        
+        Mahasiswa19 mhs2 = new Mahasiswa19();
+        mhs2.nim = 201;
+        mhs2.nama="Jokowi";
+        mhs2.alamat="Jl. Vinolia No 2A";
+        mhs2.kelas="2A";
+        mhs2.tampilBiodata();
+        
+        Mahasiswa19 mhs3 = new Mahasiswa19();
+        mhs3.nim = 301;
+        mhs3.nama="Praroro";
+        mhs3.alamat="Jl. Vinolia No 3A";
+        mhs3.kelas="3A";
+        mhs3.tampilBiodata();
+
+
+    }    
+}
