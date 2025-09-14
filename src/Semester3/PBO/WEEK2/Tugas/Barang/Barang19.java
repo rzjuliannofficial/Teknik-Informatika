26ab9325a55bfc1672c938e3a37ca30fb7800a18
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 10:25:30 +0700

    add method tampilData

--- src/Semester3/PBO/WEEK2/Tugas/Barang/Barang19.java
+++ src/Semester3/PBO/WEEK2/Tugas/Barang/Barang19.java
@@ -9,5 +9,14 @@ public class Barang19 {
     public double hitungHargaJual() {
         double diskonAmount = hargaDasar * (diskon / 100);
         return hargaDasar - diskonAmount;
+    }  
+
+    public void tampilData() {
+        System.out.println("Kode Barang: " + kode);
+        System.out.println("Nama Barang: " + namaBarang);
+        System.out.println("Harga Dasar: Rp " + hargaDasar);
+        System.out.println("Diskon: " + diskon + "%");
+        System.out.println("---------------------------------");
+        System.out.println("Harga Jual: Rp " + hitungHargaJual());
     }
 }
