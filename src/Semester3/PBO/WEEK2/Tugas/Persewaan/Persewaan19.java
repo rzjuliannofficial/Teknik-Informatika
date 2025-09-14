024080ca0e2bdac523ba7f2537c75afebdd4899c
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-14 10:05:35 +0700

    deklarasi atribut Persewaan19

---
+++ src/Semester3/PBO/WEEK2/Tugas/Persewaan/Persewaan19.java
@@ -0,0 +1,11 @@
+package Semester3.PBO.WEEK2.Tugas.Persewaan;
+
+public class Persewaan19 {
+    String idSewa;
+    String namaMember;
+    String namaGame;
+    int lamaSewa;
+    int harga;
+    int totalHarga;
+
+}
