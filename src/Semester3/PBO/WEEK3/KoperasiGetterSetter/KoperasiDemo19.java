9b8e3941ba2132b175590c425fc41bdf06e039f2
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-18 17:25:17 +0700

    add paramater

--- src/Semester3/PBO/WEEK3/KoperasiGetterSetter/KoperasiDemo19.java
+++ src/Semester3/PBO/WEEK3/KoperasiGetterSetter/KoperasiDemo19.java
@@ -3,7 +3,8 @@ package Semester3.PBO.WEEK3.KoperasiGetterSetter;
 public class KoperasiDemo19 {
 
     public static void main(String[] args) {
-        Anggota19 anggota1 = new Anggota19();
+        Anggota19 anggota1 = new Anggota19("Iwan", "Jalan Mawar");
+        System.out.println("Simpanan "+anggota1.getNama()+ " : Rp "+ anggota1.getSimpanan());
 
         anggota1.setNama("Iwan Setiawan");
         anggota1.setAlamat("Jalan Sukarno Hatta no 10");
