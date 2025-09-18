68e9092aa0f768c42cbbe4b6f9591189cc01e5ba
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-18 17:18:57 +0700

    buat object anggota1 dan inisialisasi value

---
+++ src/Semester3/PBO/WEEK3/KoperasiGetterSetter/KoperasiDemo19.java
@@ -0,0 +1,16 @@
+package Semester3.PBO.WEEK3.KoperasiGetterSetter;
+
+public class KoperasiDemo19 {
+
+    public static void main(String[] args) {
+        Anggota19 anggota1 = new Anggota19();
+
+        anggota1.setNama("Iwan Setiawan");
+        anggota1.setAlamat("Jalan Sukarno Hatta no 10");
+        anggota1.setor(100000);
+        System.out.println("Simpanan " +anggota1.getNama()+" : Rp "+ anggota1.getSimpanan());
+
+        anggota1.ambil(5000);
+        System.out.println("Simpanan " +anggota1.getNama()+" : Rp "+ anggota1.getSimpanan());    
+    }
+}
