3f5ad88fb2391282e81d162b9b8e2a8daf628c07
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-18 16:38:02 +0700

    add main - add objek motor inisialisasi kecepatan

---
+++ src/Semester3/PBO/WEEK3/MotorEncapulation/MotorDemo19.java
@@ -0,0 +1,10 @@
+package Semester3.PBO.WEEK3.MotorEncapulation;
+
+public class MotorDemo19 {
+    public static void main(String[] args) {
+        Motor19 motor = new Motor19();
+        motor.printStatus();
+        motor.kecepetan = 50;
+        motor.printStatus();
+    }
+}
