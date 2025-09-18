24aec84db02e1bb799eddbf6a65b0deda3069f32
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-18 16:45:30 +0700

    add method tambahKecepatan dan kurangiKecepatan

--- src/Semester3/PBO/WEEK3/MotorEncapulation/Motor19.java
+++ src/Semester3/PBO/WEEK3/MotorEncapulation/Motor19.java
@@ -7,10 +7,26 @@ public class Motor19 {
     public void nyalakanMesin(){
         kontakOn = true;
     }
-    
+
     public void matikanMesin(){
         kontakOn = false;
         kecepetan = 0;
+    }
+
+    public void tambahKeceptan(){
+        if (kontakOn == true) {
+            kecepetan += 5;
+        }else{
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin Off! \n");
+        }
+    }
+
+    public void kurangiKeceptan(){
+        if (kontakOn == true) {
+            kecepetan -= 5;
+        }else{
+            System.out.println("Kecepatan tidak bisa berkurang karena Mesin Off! \n");
+        }
     }
 
     public void printStatus(){
