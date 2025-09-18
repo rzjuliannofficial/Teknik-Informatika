b62eca2bf583a36667264411cf77d9851320ad7d
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-18 21:37:44 +0700

    add TestKoperasi

---
+++ src/Semester3/PBO/WEEK3/Koperasi/TestKoperasi19.java
@@ -0,0 +1,25 @@
+package Semester3.PBO.WEEK3.Koperasi;
+
+public class TestKoperasi19 {
+
+    public static void main(String[] args){
+        // int pinjam;
+        // int angsur;
+        Anggota19 donny = new Anggota19("111333444", "Donny", 5000000);
+        System.out.println("Nama Anggota: " + donny.getNama());
+        System.out.println("Limit Pinjaman: " + donny.getLimitPinjaman());
+        System.out.println("\nMeminjam uang 10.000.000...");
+        donny.pinjam(10000000);
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+        System.out.println("\nMeminjam uang 4.000.000...");
+        donny.pinjam(4000000);
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+        System.out.println("\nMembayar angsuran 1.000");
+        donny.angsur(1000);
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+        System.out.println("\nMembayar angsuran 3.000.000");
+        donny.angsur(3000000);
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+    }
+}
+
