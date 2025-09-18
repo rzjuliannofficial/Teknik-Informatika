d3712e58a0787e296a0b1e736eb7ef5b2aa40dc4
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-18 21:36:13 +0700

    push

---
+++ src/Semester3/PBO/WEEK3/EncapTest.java
@@ -0,0 +1,12 @@
+package Semester3.PBO.WEEK3;
+
+public class EncapTest {
+    public static void main(String[] args) {
+        EncapDemo encap = new EncapDemo();
+        encap.setName("James");
+        encap.setAge(18);
+
+        System.out.println("Nama : " + encap.getName());
+        System.out.println("Age : " + encap.getAge());
+    }
+}
