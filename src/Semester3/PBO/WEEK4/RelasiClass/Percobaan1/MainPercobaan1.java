e42336b3ea74134ce2def196d736fc8a9a3375fe
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-19 14:13:17 +0700

    add inisialisasi object processor pada parameter laptop19

--- src/Semester3/PBO/WEEK4/RelasiClass/Percobaan1/MainPercobaan1.java
+++ src/Semester3/PBO/WEEK4/RelasiClass/Percobaan1/MainPercobaan1.java
@@ -3,7 +3,9 @@ package Semester3.PBO.WEEK4.RelasiClass.Percobaan1;
 public class MainPercobaan1 {
     public static void main(String[] args) {
         Processor19 proc = new Processor19("Intel i5", 3);
-        Laptop19 laptop = new Laptop19("Asus", proc);
+        // Laptop19 laptop = new Laptop19("Asus", proc);
+        Laptop19 laptop = new Laptop19("Asus", new Processor19("Intel i5", 3));
+        //ada 2 cara untuk memasukkan value pada parameter di class processor19
 
         laptop.info();
         System.out.println();
