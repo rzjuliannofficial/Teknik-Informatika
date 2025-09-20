db930a449c923f38ade9b34cb35991a13bcef5d3
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-20 10:17:44 +0700

    add main

---
+++ src/Semester3/PBO/WEEK4/RelasiClass/Percobaan4/MainPercobaan4.java
@@ -0,0 +1,10 @@
+package Semester3.PBO.WEEK4.RelasiClass.Percobaan4;
+
+public class MainPercobaan4 {
+    public static void main(String[] args) {
+        Penumpang19 p = new Penumpang19("12345", "Mr. Krab");
+        Gerbong19 gerbong = new Gerbong19("A", 10);
+        gerbong.setPenumpang(p, 1);
+        System.out.println(gerbong.info());
+    }    
+}
