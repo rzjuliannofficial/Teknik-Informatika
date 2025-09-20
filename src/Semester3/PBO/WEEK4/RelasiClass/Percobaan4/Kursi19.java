4f85da9b48a55881d8b06b2535d8febe34a2fb41
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-09-20 09:57:43 +0700

    add Class kursi19

---
+++ src/Semester3/PBO/WEEK4/RelasiClass/Percobaan4/Kursi19.java
@@ -0,0 +1,33 @@
+package Semester3.PBO.WEEK4.RelasiClass.Percobaan4;
+
+public class Kursi19 {
+    String nomor;
+    Penumpang19 penumpang;
+
+    Kursi19(String nomor){
+        this.nomor = nomor;
+    }
+
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+    public String getNomor() {
+        return nomor;
+    }
+
+    public void setPenumpang(Penumpang19 penumpang) {
+        this.penumpang = penumpang;
+    }
+    public Penumpang19 getPenumpang() {
+        return penumpang;
+    }
+
+    public String info() {
+        String info = "";
+        info += "Nomor: " + nomor + "\n";
+        if (this.penumpang != null) {
+            info += "Penumpang: " + penumpang.info() + "\n";
+        }
+        return info;
+    }
+}
