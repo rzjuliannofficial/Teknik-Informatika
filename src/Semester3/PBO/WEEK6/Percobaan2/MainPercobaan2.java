95d6f5949347e46e19aebd03305aabc927c7ba19
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-03 13:59:33 +0700

    add MainPercobaan2

---
+++ src/Semester3/PBO/WEEK6/Percobaan2/MainPercobaan2.java
@@ -0,0 +1,13 @@
+package Semester3.PBO.WEEK6.Percobaan2;
+
+public class MainPercobaan2 {
+    public static void main(String[] args) {
+        ClassB hitung = new ClassB();
+        hitung.setX(20);
+        hitung.setY(30);
+        hitung.setZ(5);
+        hitung.getNilai();
+        hitung.getNilaiZ();
+        hitung.getJumlah();
+    }    
+}
