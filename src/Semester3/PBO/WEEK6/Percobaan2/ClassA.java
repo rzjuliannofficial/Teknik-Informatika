f5802c7d7dd1faf75660de383fa742bda244ef7a
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-03 13:58:57 +0700

    add ClassA

---
+++ src/Semester3/PBO/WEEK6/Percobaan2/ClassA.java
@@ -0,0 +1,17 @@
+package Semester3.PBO.WEEK6.Percobaan2;
+
+public class ClassA {
+    private int x;
+    private int y;
+
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void getNilai(){
+        System.out.println("Nilai x: "+x);
+        System.out.println("Nilai y: "+y);
+    }
+}
