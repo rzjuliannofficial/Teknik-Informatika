ebfaed2c5c7650dc6e6aa4ddec18caa8cb956f09
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-03 13:43:50 +0700

    Add ClassB

---
+++ src/Semester3/PBO/WEEK6/Percobaan1/ClassB.java
@@ -0,0 +1,13 @@
+package Semester3.PBO.WEEK6.Percobaan1;
+
+public class ClassB {
+    public int z;
+    
+    public void getNilaiZ() {
+        System.out.println("Nilai Z: "+ z);
+    }
+
+    public void getJumlah(){
+        System.out.println("Jumlah: "+(x+y+z));
+    }
+}
