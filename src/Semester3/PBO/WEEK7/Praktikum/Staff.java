41e20532a7a4327c80d0ce93feb789de533fa0b2
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-11 09:23:45 +0700

    Staff

---
+++ src/Semester3/PBO/WEEK7/Praktikum/Staff.java
@@ -0,0 +1,50 @@
+package Semester3.PBO.WEEK7.Praktikum;
+
+public class Staff extends Karyawan {
+    private int lembur;
+    private double gajiLembur;
+
+    public void setLembur (int lembur)
+    {
+        this.lembur = lembur;
+    }
+
+    public int getLembur ()
+    {
+        return lembur;
+    }
+
+    public void setGajiLembur (double gajiLembur)
+    {
+        this.gajiLembur = gajiLembur;
+    }
+
+    public double getGajiLembur ()
+    {
+        return gajiLembur;
+    }
+
+    // Overloading method getGaji dengan 2 parameter
+    public double getGaji (int lembur, double gajiLembur)
+    {
+        return super.getGaji() + lembur * gajiLembur;
+    }
+
+    // Overriding method getGaji tanpa parameter (atau Overloading, karena ada yang 2 parameter)
+    // Berdasarkan class diagram, ini adalah Overriding dari Karyawan.getGaji() dan Overloading dari getGaji(int, double).
+    @Override
+    public double getGaji ()
+    {
+        return super.getGaji() + lembur * gajiLembur;
+    }
+
+    public void lihatInfo()
+    {
+        System.out.println("NIP: " + this.getNip());
+        System.out.println("Nama: " + this.getNama());
+        System.out.println("Golongan: " + this.getGolongan());
+        System.out.println("Jml Lembur: " + this.getLembur());
+        System.out.printf("Gaji Lembur: %.0f\n", this.getGajiLembur());
+        System.out.printf("Gaji: %.0f\n", this.getGaji());
+    }
+}
