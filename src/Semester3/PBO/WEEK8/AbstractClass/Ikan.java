4bd2665004df62a6dd1a421d96df2e7a7b66c1c5
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-17 16:20:43 +0700

    add ikan

---
+++ src/Semester3/PBO/WEEK8/AbstractClass/Ikan.java
@@ -0,0 +1,9 @@
+package Semester3.PBO.WEEK8.AbstractClass;
+
+public class Ikan extends Hewan{
+    
+    @Override
+    public void bergerak() {
+        System.out.println("Berenang dengan SIRIP, \"wush..wush..\"");
+    }
+}
