7c04de7713718ab2e51e02dc0384ac61fb7e0472
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-31 13:31:27 +0700

    program

---
+++ src/Semester3/PBO/WEEK8/AbstractClass/Program.java
@@ -0,0 +1,14 @@
+package Semester3.PBO.WEEK8.AbstractClass;
+
+public class Program{
+    public static void main(String[] args){
+        Kucing kucingKampung = new Kucing();
+        Ikan lumbaLumba = new Ikan();
+        Orang ani = new Orang("Ani");
+        Orang budi = new Orang("Budi");
+        ani.peliharaHewan (kucingKampung);
+        budi.peliharaHewan (lumbaLumba);
+        ani.ajakPeliharaanJalanJalan();
+        budi.ajakPeliharaanJalanJalan();
+    }
+}
