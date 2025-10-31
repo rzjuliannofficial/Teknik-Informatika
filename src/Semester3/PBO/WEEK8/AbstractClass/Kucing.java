c7fceea9306ed82e97d8e69ecb920ba54196f846
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-31 13:31:16 +0700

    kucing

---
+++ src/Semester3/PBO/WEEK8/AbstractClass/Kucing.java
@@ -0,0 +1,9 @@
+package Semester3.PBO.WEEK8.AbstractClass;
+
+public class Kucing extends Hewan{
+
+    @Override
+    public void bergerak() {
+        System.out.println("Berjalan dengan KAKI, \"Tap..tap..\"");
+    }
+}
