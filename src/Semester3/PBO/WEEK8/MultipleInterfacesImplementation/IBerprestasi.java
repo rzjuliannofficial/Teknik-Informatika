e4d0fe485dfc352074fb4bec3884c4031b7f16f4
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-31 13:32:33 +0700

    IBerprestasi

---
+++ src/Semester3/PBO/WEEK8/MultipleInterfacesImplementation/IBerprestasi.java
@@ -0,0 +1,6 @@
+package Semester3.PBO.WEEK8.MultipleInterfacesImplementation;
+
+public interface IBerprestasi {
+    public abstract void menjuaraiKompetisi();
+    public abstract void membuatPublikasiIlmiah();
+}
